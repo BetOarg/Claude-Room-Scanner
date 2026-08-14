ececdb405c147241f77db363ba494341d6d8ac96
BetO <pepitero@gmail.com>
2026-08-14 08:18:39 -0300

    Actualizar scanner_capabilities.dart

--- lib/scanner/engine/scanner_capabilities.dart
+++ lib/scanner/engine/scanner_capabilities.dart
@@ -1,8 +1,8 @@
-/// Capacidades físicas/software relevantes para Scanner Engine.
 class ScannerCapabilities {
   final bool hasCamera;
   final bool hasArCore;
   final bool hasArKit;
+
   final bool hasGyroscope;
   final bool hasAccelerometer;
   final bool hasMagnetometer;
@@ -16,30 +16,49 @@ class ScannerCapabilities {
     this.hasMagnetometer = false,
   });
 
-  /// El dispositivo puede utilizar algún motor AR.
-  bool get supportsAR => hasArCore || hasArKit;
+  bool get supportsAR {
+    return hasArCore || hasArKit;
+  }
 
-  /// El dispositivo puede utilizar el scanner básico.
-  bool get supportsBasicScanner => hasCamera;
+  bool get supportsBasicScanner {
+    return hasCamera;
+  }
 
-  /// Nivel máximo recomendado.
-  ///
-  /// AR > BASIC > MANUAL.
+  bool get hasIMU {
+    return hasGyroscope || hasAccelerometer;
+  }
+
+  bool get hasCompleteIMU {
+    return hasGyroscope &&
+        hasAccelerometer &&
+        hasMagnetometer;
+  }
+
+  bool get supportsManualScanner {
+    return true;
+  }
+
   String get recommendedMode {
-    if (supportsAR) return 'ar';
-    if (supportsBasicScanner) return 'basic';
+    if (supportsAR) {
+      return 'ar';
+    }
+
+    if (supportsBasicScanner) {
+      return 'basic';
+    }
+
     return 'manual';
   }
 
   @override
   String toString() {
     return 'ScannerCapabilities('
-        'camera: $hasCamera, '
-        'arCore: $hasArCore, '
-        'arKit: $hasArKit, '
-        'gyro: $hasGyroscope, '
-        'accelerometer: $hasAccelerometer, '
-        'magnetometer: $hasMagnetometer'
+        'camera=$hasCamera, '
+        'arCore=$hasArCore, '
+        'arKit=$hasArKit, '
+        'gyroscope=$hasGyroscope, '
+        'accelerometer=$hasAccelerometer, '
+        'magnetometer=$hasMagnetometer'
         ')';
   }
 }
