ddb5deecc936e5bd6aca5329785f7244e122f743
BetO <pepitero@gmail.com>
2026-08-16 20:50:29 -0300

    Actualizar main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'config/supabase_config.dart';
+import 'l10n/generated/app_localizations.dart';
 import 'models/room_model.dart';
 import 'providers/scanner_provider.dart';
 import 'providers/floor_plan_provider.dart';
@@ -56,8 +58,17 @@ class RoomScannerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Room Scanner AR',
+      onGenerateTitle: (context) =>
+          AppLocalizations.of(context).appTitle,
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales:
+          AppLocalizations.supportedLocales,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blue,
