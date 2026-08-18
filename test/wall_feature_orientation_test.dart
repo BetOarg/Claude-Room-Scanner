b7ea5d610657941eae55e9662af681432933e446
BetO <pepitero@gmail.com>
2026-08-18 18:57:50 -0300

    Actualizar wall_feature_orientation_test.dart

--- test/wall_feature_orientation_test.dart
+++ test/wall_feature_orientation_test.dart
@@ -13,6 +13,20 @@ void main() {
 
       expect(feature.doorHingeSide, DoorHingeSide.start);
       expect(feature.doorSwingSide, DoorSwingSide.left);
+      expect(feature.openingHeightMeters, closeTo(2.10, 0.000001));
+      expect(feature.sillHeightMeters, closeTo(0, 0.000001));
+    });
+
+    test('restaura alturas predeterminadas para ventanas anteriores', () {
+      final feature = WallFeature.fromJson({
+        'id': 'window-1',
+        'type': 'window',
+        'start': {'x': 0, 'y': 0, 'z': 0},
+        'end': {'x': 1.2, 'y': 0, 'z': 0},
+      });
+
+      expect(feature.openingHeightMeters, closeTo(1.20, 0.000001));
+      expect(feature.sillHeightMeters, closeTo(0.90, 0.000001));
     });
 
     test('conserva bisagra y giro al exportar e importar', () {
@@ -23,12 +37,16 @@ void main() {
         end: ARPoint(x: 1.8, y: 0, z: 2),
         doorHingeSide: DoorHingeSide.end,
         doorSwingSide: DoorSwingSide.right,
+        openingHeightMeters: 2.25,
+        sillHeightMeters: 0.05,
       );
 
       final restored = WallFeature.fromJson(original.toJson());
 
       expect(restored.doorHingeSide, DoorHingeSide.end);
       expect(restored.doorSwingSide, DoorSwingSide.right);
+      expect(restored.openingHeightMeters, closeTo(2.25, 0.000001));
+      expect(restored.sillHeightMeters, closeTo(0.05, 0.000001));
     });
 
     test('copyWith cambia solamente la orientación indicada', () {
